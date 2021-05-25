0ebb99769d6f6c0942630f403dca025db89690bf
Evgeniy <ezarioexp@gmail.com>
2021-05-25 16:50:54 +0300

    added interfaces to commands database

---
+++ Pharmacy_Store/CommandDataBase/IDeleteProducts.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Pharmacy_Store.Models;
+
+namespace Pharmacy_Store.CommandDataBase
+{
+    interface IDeleteProducts
+    {
+        void DeleteProducts(Product product);
+    }
+}
